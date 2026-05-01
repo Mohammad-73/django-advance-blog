ac34d760fa40b5c1e0e7bcc478c036c08e1b67c6
Mohammad-73 <m.khalili7310@gmail.com>
2026-05-01 12:43:38 +0330

    RedirectView

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from .models import Post
 
 def indexView(request):
+    '''
+    A function based view to show index page
+    '''
     name = 'ali'
     context = {"name":name}
     return render(request, "index.html",context)
+
+class IndexView(TemplateView):
+    '''
+    A class base view to show index page
+    '''
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["name"] = "ali"
+        context["post"] = Post.object.all()
+        return context
