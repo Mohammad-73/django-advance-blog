ada7c4eaaaa2df1324059c82a6766f65b0f6c320
Mohammad-73 <m.khalili7310@gmail.com>
2026-05-01 13:14:19 +0330

    Introduction to ListView

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic import ListView
 from .models import Post
 from django.shortcuts import get_object_or_404
 
@@ -34,3 +35,12 @@ class RedirectToMaktab(RedirectView):
         post = get_object_or_404(Post, pk=kwargs['pk'])
         print(post)
         return super().get_redirect_url(*args, **kwargs)
+    
+class PostList(ListView):
+    # model = Post
+    # queryset = Post.objects.all()
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        posts = Post.objects.filter(status=True)
+        return posts
