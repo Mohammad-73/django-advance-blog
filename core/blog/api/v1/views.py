313fbd6e837eb3f825d8874e4f8412139b984a52
Mohammad-73 <m.khalili7310@gmail.com>
2026-05-05 17:01:17 +0330

    serializers

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,6 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import PostSerializer
+from ...models import Post
 
 @api_view()
 def api_post_list_view(request):
     return Response("OK")
+
+@api_view()
+def postDetail(request,id):
+    post = Post.objects.get(pk=id)
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
