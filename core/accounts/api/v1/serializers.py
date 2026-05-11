c63a7abeb575faf7a9a5de952275841764661d01
Mohammad Khalili <m.khalili7310@gmail.com>
2026-05-11 12:24:38 +0330

    Serializer

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -5,3 +5,9 @@ class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['email', 'password']
+
+    def validate(self, attrs):
+        return super().validate(attrs)
+    
+    def create(self, validated_data):
+        return super().create(validated_data)
