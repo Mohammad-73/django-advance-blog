2241c03221fd2dbd9138ed102520e1add0aa5d06
Mohammad Khalili <m.khalili7310@gmail.com>
2026-05-11 12:50:32 +0330

    Complete Registration Endpoint

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -13,4 +13,5 @@ class RegistrationAPIView(generics.GenericAPIView):
             data = {
                 'email': serializer.validated_data['email']
             }
-        return Response(data, status=status.HTTP_201_CREATED)
+            return Response(data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
