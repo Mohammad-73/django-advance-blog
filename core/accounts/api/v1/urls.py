0fcec4a2773e3c1154a4dd122e17cda6bdc24db4
Mohammad Khalili <m.khalili7310@gmail.com>
2026-05-12 09:54:17 +0330

    Implementation Token Authentication login with ObtainAuthToken

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from . import views
+from rest_framework.authtoken.views import ObtainAuthToken
 
 urlpatterns = [
     # registration
-    path('registration/', views.RegistrationAPIView.as_view(), name='registration')
+    path('registration/', views.RegistrationAPIView.as_view(), name='registration'),
+    path('token/login', ObtainAuthToken.as_view(), name='token-login')
 
     # change password
     # reset password
