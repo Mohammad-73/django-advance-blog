5eefe4e773654bbe9c4004ff5b94554e8bbac568
Mohammad Khalili <m.khalili7310@gmail.com>
2026-05-13 12:37:18 +0330

    Auth

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path, include
 from . import views
-from rest_framework.authtoken.views import ObtainAuthToken
+# from rest_framework.authtoken.views import ObtainAuthToken
 
 urlpatterns = [
     # registration
     path('registration/', views.RegistrationAPIView.as_view(), name='registration'),
-    path('token/login', ObtainAuthToken.as_view(), name='token-login')
 
     # change password
     # reset password
     # login token
+    path('token/login', views.CustomObtainAuthToken.as_view(), name='token-login'),
+    path('token/logout', views.CustomDiscardAuthToken.as_view(), name='token-logout'),
+
     # login jwt
 ]
--- core/accounts/models.py
+++ core/accounts/models.py
@@ -33,7 +33,7 @@ class UserManager(BaseUserManager):
             raise ValueError(_('Superuser must have is_staff=True.'))
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(_('Superuser must have is_superuser=True.'))
-        return self.create_user(email,password,extra_fields)
+        return self.create_user(email,password,**extra_fields)
         
 
 class User(AbstractBaseUser,PermissionsMixin):
