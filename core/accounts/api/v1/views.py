59fa4f3d95c6e7e0ff99963fd100534fbd00acd3
Mohammad Khalili <m.khalili7310@gmail.com>
2026-05-23 09:18:12 +0330

    Creating token manually

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -14,6 +14,7 @@ from django.shortcuts import get_object_or_404
 # from django.core.mail import send_mail
 from mail_templated import send_mail
 from ..utils import EmailThread
+from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
 
@@ -88,7 +89,7 @@ class ProfileAPIView(generics.RetrieveUpdateAPIView):
 
 class TestEmailSend(generics.GenericAPIView):
     
-    def post(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         # send_mail(
         #     'Subject here',
         #     'Here is the message',
@@ -96,6 +97,14 @@ class TestEmailSend(generics.GenericAPIView):
         #     ['to@example.com'],
         #     fail_silently=False
         # )
-        email_obj = EmailMessage('email/hello.tpl', {'user': 'Mohammad'}, 'admin@admin.com', to=['mkh@gmail.com'])
+        self.email = "khalili.mohammad@gmail.com"
+        user_obj = get_object_or_404(User, email = self.email)
+        token = self.get_tokens_for_user(user_obj)
+
+        email_obj = EmailMessage('email/hello.tpl', {'token': token}, 'admin@admin.com', to=[self.email])
         EmailThread.send(email_obj).start()
         return Response("email sent")
+    
+    def get_tokens_for_user(self, user):
+        refresh = RefreshToken.for_user(user)
+        return  str(refresh.access_token)
