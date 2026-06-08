b8c077a2eb50ff6a9d62869cfd0661faeac2ae73
Mohammad-73 <m.khalili7310@gmail.com>
2026-06-08 13:34:13 +0330

    Category of tests and url test

---
+++ core/blog/tests/test_blog_urls.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from .views import IndexView, PostListView, PostDetailView
+
+class TestURL(TestCase):
+    def test_blog_index_url_resolve(self):
+        url = reverse("blog:index")
+        self.assertEquals(resolve(url).func.view_class, IndexView)
+   
+    def test_blog_post_list_url_resolve(self):
+        url = reverse("blog:post-list")
+        self.assertEquals(resolve(url).func.view_class, PostListView)
+
+    def test_blog_post_detail_url_resolve(self):
+        url = reverse("blog:post-detail",kwargs={"pk":1})
+        self.assertEquals(resolve(url).func.view_class, PostDetailView)
